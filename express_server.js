648edd42799824cb6f482bc26d82df10c08470c6
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-03-21 14:50:26 -0400

    Ejs view engine

--- express_server.js
+++ express_server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+
+app.set("view engine", "ejs");
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");

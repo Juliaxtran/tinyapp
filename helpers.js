00e3e34db888ae12ad77addf38beaa26b788a6e5
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-03-24 19:32:07 -0400

    Helper Functions

---
+++ helpers.js
@@ -0,0 +1,37 @@
+
+
+
+
+const generateRandomString = () => {
+  let randomString = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz';
+  for (let i = 0; i < 6; i++) {
+    randomString += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return randomString;
+};
+
+
+
+const getUserByEmail = (email, users) => {
+  for (const userId in users) {
+    const user = users[userId];
+    if (user.email === email) {
+      return user;
+    }
+  }
+  return null;
+};
+
+const urlsForUser = (id) => {
+  const urlsForThisUser = {};
+  for (const key in urlDatabase) {
+    if (id === urlDatabase[key]["userID"]) {
+      urlsForThisUser[key] = urlDatabase[key].longURL;
+
+    }
+  }
+  return urlsForThisUser;
+};
+
+module.exports = { generateRandomString, getUserByEmail, urlsForUser};
